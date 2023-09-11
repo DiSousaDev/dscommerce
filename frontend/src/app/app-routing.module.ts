7575b8f3e573aa4adccd73a8f9dad8c51c700cd5
Eder Diego de Sousa <me@diego.dev.br>
2023-09-11 01:02:54 -0300

    Add routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCatalogComponent } from "./features/commerce/pages/product-catalog/product-catalog.component";
+import { ProductDetailsComponent } from "./features/commerce/pages/product-details/product-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products/catalog',
+    pathMatch: 'full'
+  },
+  {
+    path: 'products',
+    redirectTo: 'products/catalog'
+  },
+  {
+    path: 'products/catalog',
+    component: ProductCatalogComponent
+  },
+  {
+    path: 'products/details',
+    component: ProductDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
