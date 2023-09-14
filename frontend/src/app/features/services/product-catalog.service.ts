87f6320f00b1476d5f1b0433a371b056795beae5
Eder Diego de Sousa <me@diego.dev.br>
2023-09-14 00:24:29 -0300

    Catch request error

--- frontend/src/app/features/services/product-catalog.service.ts
+++ frontend/src/app/features/services/product-catalog.service.ts
@@ -2,27 +2,35 @@ import { Injectable } from '@angular/core';
 import { Product } from "../../core/models/product.model";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
-import { Observable } from "rxjs";
+import { catchError, EMPTY, Observable } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductCatalogService {
 
-  private url = `${environment.api}/products`;
-  private urlSize12 = `${environment.api}/products?size=12`;
+    private url = `${environment.api}/products`;
+    private urlSize12 = `${environment.api}/products?size=12`;
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+    constructor(
+        private httpClient: HttpClient,
+        private router: Router
+    ) {
+    }
 
-  findAll(){
-    return this.httpClient.get<any>(this.urlSize12);
-  }
+    findAll() {
+        return this.httpClient.get<any>(this.urlSize12);
+    }
 
-  findById(id: number) : Observable<Product> {
-    const url = `${this.url}/${id}`;
-    return this.httpClient.get<Product>(url);
-  }
+    findById(id: number): Observable<Product> {
+        const url = `${this.url}/${id}`;
+        return this.httpClient.get<Product>(url).pipe(
+            catchError(() => {
+                this.router.navigate(["/products/catalog"]);
+                return EMPTY
+            })
+        );
+    }
 
 }
