82183e2455ea20e8e85b85bd4e919c8e3f39e771
Eder Diego de Sousa <me@diego.dev.br>
2023-09-24 01:17:31 -0300

    Cart insert item

--- frontend/src/app/features/commerce/pages/product-details/product-details.component.ts
+++ frontend/src/app/features/commerce/pages/product-details/product-details.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Product } from "../../../../core/models/product.model";
 import { ProductCatalogService } from "../../../services/product-catalog.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { ProductDto } from "../../../../core/models/product-dto.model";
+import { ItemCart } from "../../../../core/models/item-cart.model";
+import { OrderService } from "../../../services/order.service";
 
 @Component({
   selector: 'product-details',
@@ -19,15 +21,32 @@ export class ProductDetailsComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductCatalogService
+    private router: Router,
+    private productService: ProductCatalogService,
+    private orderService: OrderService
   ) {
   }
 
   ngOnInit(): void {
     // snapshot.params de ActivatedRoute dá acesso aos parâmetros passados
     let id = this.route.snapshot.params['id'];
-   this.productService.findById(id).subscribe(prod => this.product = prod);
-   this.productService.findAll().subscribe(resp => this.products = resp.content);
+    this.productService.findById(id).subscribe(prod => this.product = prod);
+    this.productService.findAll().subscribe(resp => this.products = resp.content);
+  }
+
+  addProduct(product: Product) {
+    const cart = this.orderService.get();
+    const item = cart.items.find(x => x.productId === product.id);
+    if(!item) {
+      const newItem = new ItemCart(product.id, 1, product.name, product.price, product.imgUrl);
+      cart.items.push(newItem);
+      this.orderService.save(cart);
+    }
+  }
+
+  handleBuyClick(product: Product): void {
+    this.addProduct(product);
+    this.router.navigate(['/cart']);
   }
 
 }
--- frontend/src/app/features/services/order.service.ts
+++ frontend/src/app/features/services/order.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Order } from "../../core/models/order.model";
+import { environment } from "../../../environments/environment";
+import { ItemCart } from "../../core/models/item-cart.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,18 @@ export class OrderService {
 
   save(cart: Order) {
     const str = JSON.stringify(cart);
-    localStorage.setItem("dscommerce/Cart", str);
+    localStorage.setItem(environment.cartKey, str);
   }
 
   get() : Order {
-    const str = localStorage.getItem("dscommerce/Cart") || '{"items" = []}';
-    return JSON.parse(str);
+    const str = localStorage.getItem(environment.cartKey) || '{"items":[]}';
+    const obj = JSON.parse(str) as Order;
+
+    const cart = new Order();
+    obj.items.forEach(x => {
+      cart.items.push(new ItemCart(x.productId, x.quantity, x.name, x.price, x.imgUrl));
+    });
+    return cart;
   }
 
 }
